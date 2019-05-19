89c1248035e00245935355fa0f470b2a464a54c8
Hyphena <serenelunation@gmail.com>
2019-05-19 09:12:23 -0400

    Created Day 2 folder Created a template for further useage Added folders to .gitignore

---
+++ Challenges/Day 2/Program.cs
@@ -0,0 +1,29 @@
+using System;
+using System.IO;
+
+namespace Day_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyLibrary myLib = new MyLibrary();
+            string input = myLib.GetInput();
+            
+            ASide(input);
+            BSide(input);
+        }
+
+
+        static void ASide(string input)
+        {
+            //TODO: complete side a
+        }
+
+
+        static void BSide(string input)
+        {
+            //TODO: complete side b
+        }
+    }
+}
