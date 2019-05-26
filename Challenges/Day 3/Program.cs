e0fd3d060eba7cff7570d956aa6413fdcaa0b0ab
Hyphena <serenelunation@gmail.com>
2019-05-26 13:25:38 -0400

    Completed Day 3A Might want to optimize algorithm later Logging each house position and gifts delivered overflows the console Added the day's obj and bin folders to .gitignore

--- Challenges/Day 3/Program.cs
+++ Challenges/Day 3/Program.cs
@@ -1,8 +1,29 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace Day_3
 {
+    class House
+    {
+        public int x, y;
+        public int counter = 0;
+
+        public House()
+        {
+            x = 0;
+            y = 0;
+            counter = 0;
+        }
+
+        public House(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+            counter++;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
@@ -16,7 +37,61 @@ namespace Day_3
 
         static void ASide(string input)
         {
-            //TODO: Complete Part 1
+            List<House> houses = new List<House>();
+            int x = 0;
+            int y = 0;
+            bool houseExists = false;
+
+
+            houses.Add(new House(0, 0));
+            foreach (char dir in input)
+            {
+                if (dir == '^')
+                    y++;
+                if (dir == '>')
+                    x++;
+                if (dir == 'v')
+                    y--;
+                if (dir == '<')
+                    x--;
+
+
+                houseExists = false;
+                foreach (House house in houses)
+                {
+                    if (house.x == x && house.y == y)
+                    {
+                        houseExists = true;
+                        house.counter++;
+                        break;
+                    }
+                }
+
+                if (!houseExists)
+                {
+                    houses.Add(new House(x, y));
+                }
+            }
+
+
+            foreach (House house in houses)
+            {
+                if (house.x == x && house.y == y)
+                {
+                    houseExists = true;
+                    house.counter++;
+                    break;
+                }
+            }
+
+
+            foreach (House house in houses)
+            {
+                Console.WriteLine("X: " + house.x.ToString() + "   Y: " + house.y.ToString() + "   Deliveries: " + house.counter);
+            }
+
+
+            Console.WriteLine("\nTotal Houses Delivered To: " + houses.Count);
         }
 
 
