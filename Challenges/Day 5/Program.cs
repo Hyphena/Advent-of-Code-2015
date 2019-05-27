0c6b0f5f826fca693271a69412d31c67aa90e6d5
Hyphena <serenelunation@gmail.com>
2019-05-27 00:15:38 -0400

    Created Day 5 folder

---
+++ Challenges/Day 5/Program.cs
@@ -0,0 +1,39 @@
+using System;
+using System.IO;
+
+namespace Day_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = GetInput();
+            
+            ASide(input);
+            BSide(input);
+        }
+
+
+        static void ASide(string input)
+        {
+            //TODO: Complete Part 1
+        }
+
+
+        static void BSide(string input)
+        {
+            //TODO: Complete Part 2
+        }
+
+
+        static string GetInput()
+        {
+            string fileContent = "[Empty File]";
+
+            if (File.Exists("Input.txt"))
+                fileContent = File.ReadAllText("Input.txt");
+
+            return fileContent;
+        }
+    }
+}
